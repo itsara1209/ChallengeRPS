0489f6d2c99510e7ba618b68e57a4f9c7cdf0fbd
ItsaraChai <kolit2011@live.com>
2017-05-31 21:07:58 +0700

    New project

---
+++ ChallengeRPS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChallengeRPS
+//
+//  Created by Itsaraporn Chaichayanon on 5/31/17.
+//  Copyright © 2017 ItsaraChai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
